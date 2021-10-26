8765614f03495be4761df239e7d42c51024ea152
1802808UT <1802408@utrivieramaya.edu.mx>
2021-10-26 01:40:20 -0500

    Update login + Update header + Update Register
    Se actualizo el login usando material.
    Se dejo más llamativo el header (se escondieron los botones por el momento).
    Se actualizo el form del registro usando material y css
    
    Co-Authored-By: cristian Antonio Ibarra <73759216+ReDSeriouShola@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import {ReactiveFormsModule} from '@angular/forms';
 /** GENERALES */
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -44,6 +45,7 @@ import { FooterComponent } from './shared/components/footer/footer.component';
     MatInputModule,
     MatButtonModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+  hide1 = true;
+  
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 }
