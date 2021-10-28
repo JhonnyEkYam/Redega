c54975299c237f61b353fdaaebbe9af905292100
Jonyaey <54909917+JhonnyEkYam@users.noreply.github.com>
2021-10-27 20:39:22 -0500

    USUARIO.PROFILE - ALL v1.0.0

--- src/app/usuario/usuario-profile/usuario-profile.component.ts
+++ src/app/usuario/usuario-profile/usuario-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-usuario-profile',
   templateUrl: './usuario-profile.component.html',
@@ -10,6 +11,18 @@ export class UsuarioProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+  hide1 = true;
+  
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
 }
