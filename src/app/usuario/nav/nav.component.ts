254c82dbe10186413e2f10d3c408fad9d742fe59
Jonyaey <54909917+JhonnyEkYam@users.noreply.github.com>
2021-11-02 21:24:51 -0500

    USUARIO.GASTOS v2.0.0

--- src/app/usuario/nav/nav.component.ts
+++ src/app/usuario/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
+  goToHome(){
+    this.router.navigate(['usuario/home']);
+  }
+  goToProfile(){
+    this.router.navigate(['usuario/profile']);
+  }
+  goToOutcomes(){
+    this.router.navigate(['usuario/gastos']);
+  }
 
+  closeSession(){
+    this.router.navigate(['/']);
+  }
 }
