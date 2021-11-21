cf2e52daf105dc98977f3c0512ff657afdc9c156
Jonyaey <54909917+JhonnyEkYam@users.noreply.github.com>
2021-11-20 22:50:20 -0500

    v0.2
    Back del sistema CRUD de Contador para los Gastos,
    Los gastos se pueden cambiar a "inactivos", pero nunca borrarse, y también se puede ver los gastos inactivos

--- src/app/general/testing/testing.component.ts
+++ src/app/general/testing/testing.component.ts
@@ -1,8 +1,8 @@
-import {Observable, Timestamp} from "rxjs";
+import { Observable, Timestamp } from "rxjs";
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from "@angular/fire/compat/firestore";
-import {NewOutcomeComponent} from "./new-outcome/new-outcome.component";
-import {MatDialog} from '@angular/material/dialog';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from "@angular/fire/compat/firestore";
+import { NewOutcomeComponent } from "./new-outcome/new-outcome.component";
+import { MatDialog } from '@angular/material/dialog';
 import { EditOutcomeComponent } from "./edit-outcome/edit-outcome.component";
 
 @Component({
@@ -12,40 +12,48 @@ import { EditOutcomeComponent } from "./edit-outcome/edit-outcome.component";
 })
 export class TestingComponent implements OnInit {
   outcomes: Observable<any[]>;
-  
+  // private readonly itemsRef: AngularFirestoreCollection<Item>;
+  // private readonly profileRef: AngularFirestoreDocument<Profile>;
+  // observables for template
+  // items: Observable<Item[]>;
+  // profile: Observable<Profile>;
   private store: AngularFirestore;
 
   constructor(public dialog: MatDialog, store: AngularFirestore) {
     this.store = store;
-    this.outcomes = store.collection('outcomes').valueChanges({
+    
+    this.outcomes = this.store.collection('outcomes', ref=>ref.where('status', '==', 1)).valueChanges({
       idField: 'id',
     });
-    // this.outcomes.forEach(outcome => {
-    //   console.log(outcome);
-    // });
+    // this.itemsRef = this.store.collection('items', ref => ref.where('user', '==', 'davideast').limit(10));
+    // this.items = this.itemsRef.valueChanges().map(snap => snap.docs.map(data => doc.data()));
+    // this.items = from(this.itemsRef); // you can also do this with no mapping
+    // this.profileRef = afs.doc('users/davideast');
+    // this.profile = this.profileRef.valueChanges();
   }
+  
+  ngOnInit() {}
 
   showCreateDialog(): void {
     const dialogRef = this.dialog.open(NewOutcomeComponent, {
       width: '600px'
     });
-    
+
     dialogRef.afterClosed().subscribe(newOutcome => {
       newOutcome.date_log = new Date();
       newOutcome.date_update = new Date();
       newOutcome.photo = 'default.jpg';
+      newOutcome.status = '1';
       this.store.collection('outcomes').add(newOutcome);
     });
   }
 
   showEditDialog(outcome: any): void {
-    // console.log(outcome)
     const dialogRef = this.dialog.open(EditOutcomeComponent, {
       width: '600px'
     });
     dialogRef.componentInstance.editedOutcome = outcome;
     dialogRef.afterClosed().subscribe(editedOutcome => {
-      // editedOutcome.date_update = new Date();
       const updatedDoc = this.store.collection('outcomes');
       updatedDoc.doc(editedOutcome.id).update({
         amount: editedOutcome.amount,
@@ -56,11 +64,19 @@ export class TestingComponent implements OnInit {
       });
     });
   }
-  public printDate(dateOrTimestamps: any): any{
-    if(dateOrTimestamps.seconds) return new Date(dateOrTimestamps.seconds*1000).toDateString();
-    return dateOrTimestamps.toDateString();
+  deleteLog(idOutcome: string): void {
+    const updatedDoc = this.store.collection('outcomes');
+    updatedDoc.doc(idOutcome).update({
+      status: 0
+    });
   }
-  private toTimestamp(strDate: any): any{ var datum = Date.parse(strDate); return datum/1000;}
-  ngOnInit(): void {
+  seeDeleted(){
+    this.outcomes = this.store.collection('outcomes', ref=>ref.where('status', '==', 0)).valueChanges({
+      idField: 'id',
+    });
+  }
+  public printDate(dateOrTimestamps: any): any {
+    if (dateOrTimestamps.seconds) return new Date(dateOrTimestamps.seconds * 1000).toDateString();
+    return dateOrTimestamps.toDateString();
   }
 }
